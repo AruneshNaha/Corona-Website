50c6d3c82d0615f5b56a5d09e7724f360e9c3ca0
Arunesh Naha <aruneshnaha18@gmail.com>
2020-08-12 22:24:50 +0530

    Updates

---
+++ precache-manifest.ad0eefa5505c40161a72fb98722f11c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c236b2d331cddd817e0b3acefd9d2c",
+    "url": "/Corona-react-website/index.html"
+  },
+  {
+    "revision": "3f28857fedb0a92484b7",
+    "url": "/Corona-react-website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b06a9d2e8d04a70b4b4e",
+    "url": "/Corona-react-website/static/css/main.96e68ba3.chunk.css"
+  },
+  {
+    "revision": "3f28857fedb0a92484b7",
+    "url": "/Corona-react-website/static/js/2.21e288f1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Corona-react-website/static/js/2.21e288f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06a9d2e8d04a70b4b4e",
+    "url": "/Corona-react-website/static/js/main.03fb69ad.chunk.js"
+  },
+  {
+    "revision": "03551bd3b2a8179a40c6",
+    "url": "/Corona-react-website/static/js/runtime-main.56a2de89.js"
+  }
+]);
